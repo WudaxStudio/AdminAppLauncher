2f4764077b2764499b1d305e6f4458fdd05a0b58
WudaxStudio <130849175+WudaxStudio@users.noreply.github.com>
2026-08-19 17:30:27 +0800

    Add AssemblyInfo.cs

---
+++ AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("AdminAppLauncher")]
+[assembly: AssemblyDescription("管理员应用启动器")]
+[assembly: AssemblyCompany("Wudax Studio")]
+[assembly: AssemblyProduct("AdminAppLauncher")]
+[assembly: AssemblyCopyright("Copyright © Wudax 2026")]
+[assembly: AssemblyTrademark("Wudax Studio")]
+[assembly: AssemblyVersion("1.1.2.0")]
+[assembly: AssemblyFileVersion("1.1.2.0")]
+[assembly: AssemblyInformationalVersion("1.1.2.0")]
